ee76154fcda3e0993327bd2dc465bf96684b84a2
Atul Kamal <atulkamal.verma@gmail.com>
2022-07-26 15:53:23 +0530

    second

--- my-personal-website/src/components/data/user-details.js
+++ my-personal-website/src/components/data/user-details.js
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import users from "./user-data";
+import { useState,useContext,useEffect } from "react";
+
 import { useParams } from "react-router-dom";
 import { BsHeart  } from 'react-icons/bs';
 import LikeContext from "../../context/LikeContext";
@@ -7,28 +7,55 @@ import LikeContext from "../../context/LikeContext";
 
 
 const UserDetails=()=>{
+
+    
+
     const params=useParams();
     // const [searchParam,setSearchParam]=useSearchParams();
     const {userId} =params;
 
     const {incrementLike}=useContext(LikeContext);
+    const [userData,setUserData]=useState(null);
+    const [loader,showLoader]=useState(false);
 
 
+    
+
+
+  useEffect(()=>{
+    showLoader(true);
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.json())
+    .then(jsonResponse=>{
+      setUserData(jsonResponse)
+      showLoader(false);
+    })
+    
+  },[])
    
-    const getUserName=()=>{
-      const userData=users.filter(({id})=>id===userId);
-      if(!userData || userData.length===0){
+    const getUser=()=>{
+      const user=userData.find(({id})=>id.toString()===userId);
+      if(!user){
         return 'user not found'
       }
-        return userData[0].name;
+        return user;
     };
+
+    if(!userData || loader){
+      return <>Loading...</>
+    }
+
+
+    const user=getUser();
 
 
     return(
        <div className="p-5 mt-5 bg-light">
 
         <h4>User Details</h4>
-        <div>UserName: {getUserName()}</div>
+        <div>UserName: {user.name}</div>
+        <div>Email: {user.email}</div>
+
         <button onClick={incrementLike} className=" mt-3 btn btn-primary">
           <BsHeart/> Like
         </button>
